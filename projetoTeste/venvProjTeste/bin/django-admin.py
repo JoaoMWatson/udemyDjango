6fe3d5fdefefdb7590e4cb182b7e974f374dc1c6
Joao Pedro <joaopedrowatson@hormail.com>
2019-10-31 11:27:36 -0300

    primeira aula

---
+++ projetoTeste/venvProjTeste/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/useless_guy/git/udemyDjango/projetoTeste/venvProjTeste/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
